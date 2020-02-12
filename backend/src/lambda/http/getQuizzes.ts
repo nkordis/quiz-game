ed5c8d18c22dd91083cd9a6acc85017b5425eadb
nikos <nikoskordis@gmail.com>
2020-02-12 18:10:10 +0100

    feat: Define the initial serverless enviroment

---
+++ backend/src/lambda/http/getQuizzes.ts
@@ -0,0 +1,19 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+        console.log('Processing event: ', event);
+
+        
+    return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          items:[]
+        })
+      }
+
+}
